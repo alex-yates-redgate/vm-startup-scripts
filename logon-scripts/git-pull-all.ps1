0a64d675fb7e5cb69e620011ffab37dba4a560d4
Alex Yates (Redgate) <140410709+alex-yates-redgate@users.noreply.github.com>
2025-02-26 11:27:12 +0000

    Adding support for putting git repos in either a "Demos" or an "Admin" subfolder.

--- logon-scripts/git-pull-all.ps1
+++ logon-scripts/git-pull-all.ps1
@@ -1,17 +1,26 @@
-$gitParentDir = "C:\git"
+$gitParentDirs = @(
+    "C:\git",
+    "C:\git\Demos",
+    "C:\git\Admin"
+)
 
-# Get all child directories in the parent directory
-$childDirectories = Get-ChildItem -Path $gitParentDir -Directory
+Foreach ($parentDir in $gitParentDirs){
+    Write-Host "Performing a git pull on all the repos in: $parentDir"
 
-Foreach ($dir in $childDirectories) {
-    $repoPath = $dir.FullName
+    # Get all child directories in the parent directory
+    $childDirectories = Get-ChildItem -Path $parentDir -Directory
 
-    # Check if the .git directory exists in the repository path
-    if (Test-Path -Path "$repoPath\.git") {
-        Write-Host "Pulling changes for repository: $repoPath"
-        # Execute git pull in the repository path
-        & git -C $repoPath pull
-    } else {
-        Write-Host "No .git directory found in: $repoPath"
+    Foreach ($dir in $childDirectories) {
+        $repoPath = $dir.FullName
+
+        # Check if the .git directory exists in the repository path
+        if (Test-Path -Path "$repoPath\.git") {
+            Write-Host "- ${repoPath}: .git file found. Pulling changes..."
+            # Execute git pull in the repository path
+            & git -C $repoPath pull | out-null
+        } else {
+            Write-Host "- ${repoPath}: .git not file found. Skipping..."
+        }
     }
+    Write-Host ""
 }
