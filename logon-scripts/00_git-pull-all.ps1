87a635b0bcedfbb73155e6b9663cdbba3a70462d
Chris Hawkins <94678272+csnhawkins@users.noreply.github.com>
2025-09-02 14:02:51 +0100

    Added logic to do a forced reset if pull doesn't work

--- logon-scripts/00_git-pull-all.ps1
+++ logon-scripts/00_git-pull-all.ps1
@@ -14,13 +14,18 @@ Foreach ($parentDir in $gitParentDirs){
         $repoPath = $dir.FullName
 
         # Check if the .git directory exists in the repository path
-        if (Test-Path -Path "$repoPath\.git") {
-            Write-Host "- ${repoPath}: .git file found. Pulling changes..."
-            # Execute git pull in the repository path
-            & git -C $repoPath pull | out-null
-        } else {
-            Write-Host "- ${repoPath}: .git not file found. Skipping..."
-        }
+		if (Test-Path -Path "$repoPath\.git") {
+			Write-Host "- ${repoPath}: .git file found. Pulling changes..."
+			& git -C $repoPath pull | Out-Null
+			if ($LASTEXITCODE -ne 0) {
+				Write-Host "- ${repoPath}: Pull failed, forcing reset to latest remote branch..."
+				$branch = & git -C $repoPath rev-parse --abbrev-ref HEAD
+				& git -C $repoPath fetch --all
+				& git -C $repoPath reset --hard origin/$branch
+			}
+		} else {
+			Write-Host "- ${repoPath}: .git not file found. Skipping..."
+		}
     }
     Write-Host ""
 }
